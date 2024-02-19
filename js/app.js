378a228e28bdb76c4edae8d47fa15c054bbd03b8
Kauas1 <kauafidelis7@gmail.com>
2024-02-19 08:44:47 -0300

    Finalizei o código

--- js/app.js
+++ js/app.js
@@ -1,1 +1,51 @@
-// alert('hello world')
+document.addEventListener('DOMContentLoaded', () => {
+    const url = "https://api.chucknorris.io/jokes/categories"
+
+    fetch(url).then((response) => {
+        if(!response.ok){
+            throw new Error('Erro ao receber os dados')
+        }
+        return response.json()
+    })
+    .then((data) =>{
+        gerarCatergorias(data)
+    })
+    .catch((err) => console.error(err))
+})
+
+function gerarCatergorias(categorias){
+    //parâmetro categorias = array de cartegorias
+    console.log(categorias)
+
+    const select = document.getElementById('select')
+    categorias.map((categoria) =>{
+         const options = document.createElement('option')
+         options.innerHTML = `${categoria}`
+         options.value
+         select.appendChild(options) 
+    })
+}
+
+const btn = document.getElementById('btn')
+btn.addEventListener('click', () => {
+    const select = document.getElementById('select').value
+    // console.log(select)
+    const url = `https://api.chucknorris.io/jokes/random?category=${select}`
+    fetch(url).then((response) => {
+        if(!response.ok){
+            throw new Error('Erro ao Seleciona uma piada')
+        }
+        return response.json()
+    })
+    .then((data) => {
+        gerarPiada(data)
+    })
+    .catch((err)=>console.error(err))
+})
+
+function gerarPiada(piada){
+        const joke = document.getElementById("joke")
+        joke.innerHTML = piada.value
+}
+
+
